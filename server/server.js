22762d3a39e6c799a4d6280f8a2ed3bf6142448d
Hunter Saunders <hjvs98@gmail.com>
2022-06-21 11:04:47 -0600

    ready for deployment

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path"); //heroku isn't smart enough so this connects our file structure
+
+const app = express();
+app.use(cors());
+app.use(express.json()); //without this express doens't know how to accept .json correctly
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+const port = process.env.PORT || 3005;
+
+app.listen(port, () => {
+  console.log(`I'll be right by your side, 'til ${port}`);
+});
