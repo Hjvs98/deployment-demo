d2c9473bc13704d823f4acc5bc3dad602058ef49
Hunter Saunders <hjvs98@gmail.com>
2022-06-21 11:38:45 -0600

    ready for second deployment

--- server/server.js
+++ server/server.js
@@ -7,7 +7,10 @@ app.use(cors());
 app.use(express.json()); //without this express doens't know how to accept .json correctly
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+app.get("/styles", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.css"));
 });
 
 const port = process.env.PORT || 3005;
